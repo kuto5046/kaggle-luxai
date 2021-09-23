6256659c58335b83410272f4d7a008fe18164f5b
Kyohei Uto <h27.kubb@gmail.com>
2021-09-23 09:29:42 +0900

    wandbフォルダを除外するオプションを追加

--- make_submissionfile.sh
+++ make_submissionfile.sh
@@ -4,5 +4,5 @@ cd `dirname $0`
 cd $1
 cp -r ~/work/input/lux-ai-2021/lux .
 cp -r ~/work/input/lux-ai-2021/main.py .
-tar --exclude='*.ipynb' --exclude="*.tar.gz" --exclude="*.log" -czf submission.tar.gz *
+tar --exclude='*.ipynb' --exclude="*.tar.gz" --exclude="*.log" --exclude="wandb" -czf submission.tar.gz *
 cd `dirname $0`
