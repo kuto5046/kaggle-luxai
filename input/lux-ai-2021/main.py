d64e130303a3579a6054ca5ea190de994a9b15ee
Kyohei Uto <h27.kubb@gmail.com>
2021-09-28 21:07:29 +0900

    fixed main.py

--- input/lux-ai-2021/main.py
+++ input/lux-ai-2021/main.py
@@ -1,6 +1,8 @@
 from typing import Dict
 import sys
 from agent import agent
+
+
 if __name__ == "__main__":
     
     def read_input():
@@ -11,12 +13,14 @@ if __name__ == "__main__":
             return input()
         except EOFError as eof:
             raise SystemExit(eof)
+
     step = 0
     class Observation(Dict[str, any]):
         def __init__(self, player=0) -> None:
             self.player = player
             # self.updates = []
             # self.step = 0
+    
     observation = Observation()
     observation["updates"] = []
     observation["step"] = 0
@@ -24,11 +28,13 @@ if __name__ == "__main__":
     while True:
         inputs = read_input()
         observation["updates"].append(inputs)
-        
-        if step == 0:
-            player_id = int(observation["updates"][0])
-            observation.player = player_id
+
+        # [FIXED]reference: https://www.kaggle.com/huikang/lux-ai-agent-evaluation
         if inputs == "D_DONE":
+            if step == 0:
+                player_id = int(observation["updates"][0])
+                observation["player"] = player_id
+                observation["width"], observation["height"] = map(int, observation["updates"][1].split())
             actions = agent(observation, None)
             observation["updates"] = []
             step += 1
