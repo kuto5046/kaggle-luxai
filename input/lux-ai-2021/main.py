2cff75309fda98553e5183c085f6d454b2eae62c
Kyohei Uto <h27.kubb@gmail.com>
2021-10-03 18:59:25 +0900

    validation errorが発生したので元に戻す

--- input/lux-ai-2021/main.py
+++ input/lux-ai-2021/main.py
@@ -1,8 +1,6 @@
 from typing import Dict
 import sys
 from agent import agent
-
-
 if __name__ == "__main__":
     
     def read_input():
@@ -13,14 +11,12 @@ if __name__ == "__main__":
             return input()
         except EOFError as eof:
             raise SystemExit(eof)
-
     step = 0
     class Observation(Dict[str, any]):
         def __init__(self, player=0) -> None:
             self.player = player
             # self.updates = []
             # self.step = 0
-    
     observation = Observation()
     observation["updates"] = []
     observation["step"] = 0
@@ -28,13 +24,11 @@ if __name__ == "__main__":
     while True:
         inputs = read_input()
         observation["updates"].append(inputs)
-
-        # [FIXED]reference: https://www.kaggle.com/huikang/lux-ai-agent-evaluation
+        
+        if step == 0:
+            player_id = int(observation["updates"][0])
+            observation.player = player_id
         if inputs == "D_DONE":
-            if step == 0:
-                player_id = int(observation["updates"][0])
-                observation["player"] = player_id
-                observation["width"], observation["height"] = map(int, observation["updates"][1].split())
             actions = agent(observation, None)
             observation["updates"] = []
             step += 1
