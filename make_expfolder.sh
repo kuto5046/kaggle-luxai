aeb34bd6b59c30f83c7bf00e5d5b0e654813b3c8
Kyohei Uto <h27.kubb@gmail.com>
2021-10-19 10:04:24 +0900

    add config

--- make_expfolder.sh
+++ make_expfolder.sh
@@ -1,5 +1,10 @@
 # $1=exp-name
+# $2= flag rl or None
 cd `dirname $0`
 mkdir ~/work/exp/$1
 cp ~/work/exp/$2/agent.py ~/work/exp/$1
 cp ~/work/exp/$2/train.py ~/work/exp/$1
+
+if [ $2 =="rl" ]; then
+    cp ~/work/LuxPythonEnvGym/lixai2021 ~/work/exp/$1
+fi
