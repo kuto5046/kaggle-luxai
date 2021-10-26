860bc2b36cc0cf5d6c7e6a3c10248c78efdeb785
Kyohei Uto <h27.kubb@gmail.com>
2021-10-26 20:11:30 +0900

    add imitation agents & resume function

--- exp/exp028/train.py
+++ exp/exp028/train.py
@@ -19,13 +19,15 @@ from stable_baselines3.common.vec_env import SubprocVecEnv
 from wandb.integration.sb3 import WandbCallback
 
 from agent_policy import AgentPolicy
+from agents.imitation.agent_policy import ImitationAgent
+from agents.random.agent_policy import RandomAgent
 from luxai2021.env.agent import Agent
 from luxai2021.env.lux_env import LuxEnvironment, SaveReplayAndModelCallback
 from luxai2021.game.constants import LuxMatchConfigs_Default
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.logger import Logger, configure
 from luxai2021.game.game import Game
-        
+
 
 def get_logger(level=INFO, out_file=None):
     logger = logging.getLogger()
@@ -90,22 +92,30 @@ def main():
     n_steps = config["basic"]["n_steps"]
     n_envs = config["basic"]["n_envs"]
     step_count = config["basic"]["step_count"]
-    resume = config["basic"]["resume"]
     pretrained_path = config["basic"]["pretrained_path"]
     model_update_step_freq = config["basic"]["model_update_step_freq"]
+
+    is_resume = config['resume']['is_resume']
+    resume_num_timesteps = config['resume']['resume_num_timesteps']
+    run_id = config['resume']['run_id']
+
     ckpt_params = config['callbacks']['checkpoints']
     eval_params = config['callbacks']['eval']
     model_params = config["model"]
     seed_everything(seed)
     EXP_NAME = str(Path().resolve()).split('/')[-1]
+    
+    if not is_resume:
+        run_id = None 
 
     run = wandb.init(
         project='lux-ai', 
         entity='kuto5046', 
         config=config, 
         group=EXP_NAME, 
+        id = run_id,
         sync_tensorboard=True,# auto-upload sb3's tensorboard metrics
-        monitor_gym=True,  # auto-upload the videos of agents playing the game
+        monitor_gym=False,  # auto-upload the videos of agents playing the game
         save_code=False,  # optional
     )
     # Run a training job
@@ -115,44 +125,46 @@ def main():
     #    agent
     ##############
     # Create a default opponent agent and a RL agent in training mode
+    
+    opponents = {
+        "random": RandomAgent(),
+        "imitation": ImitationAgent(),
+    }
 
-    if resume:
-        # agent
+    if os.path.exists(pretrained_path):
         old_model = PPO.load(pretrained_path, device="cpu")
-        opponent = AgentPolicy(mode="inference", model=old_model)
+
+        # Create a default opponent agent and a RL agent in training mode
+        opponents["self-play"] = AgentPolicy(mode="inference", model=old_model)
+
         player = AgentPolicy(mode="train", model=old_model)
-
-        #  environmnet
+        # environmnet
         if n_envs == 1:
             env = LuxEnvironment(configs=configs,
                                 learning_agent=player,
-                                opponent_agent=opponent, 
+                                opponent_agents=opponents, 
                                 model_update_step_freq=model_update_step_freq)
         else:
             env = SubprocVecEnv([make_env(LuxEnvironment(configs=configs,
-                                                         learning_agent=AgentPolicy(mode="train", model=old_model),
-                                                         opponent_agent=opponent,
-                                                         model_update_step_freq=model_update_step_freq), i) for i in range(n_envs)])
-        model = PPO("MlpPolicy", env, **model_params) 
+                                                            learning_agent=AgentPolicy(mode="train", model=old_model),
+                                                            opponent_agents=opponents,
+                                                            model_update_step_freq=model_update_step_freq), i) for i in range(n_envs)])
     else:
-        # agent
-        opponent = Agent()
+        opponents["self-play"] = AgentPolicy(mode="inference")
         player = AgentPolicy(mode="train")
-
         #  environmnet
         if n_envs == 1:
             env = LuxEnvironment(configs=configs,
                                 learning_agent=player,
-                                opponent_agent=opponent,
+                                opponent_agents=opponents, 
                                 model_update_step_freq=model_update_step_freq)
         else:
             env = SubprocVecEnv([make_env(LuxEnvironment(configs=configs,
-                                                         learning_agent=AgentPolicy(mode="train"),
-                                                         opponent_agent=opponent,
-                                                         model_update_step_freq=model_update_step_freq), i) for i in range(n_envs)])
- 
-        # model
-        model = PPO("MlpPolicy", env, **model_params) 
+                                                            learning_agent=AgentPolicy(mode="train"),
+                                                            opponent_agents=opponents,
+                                                            model_update_step_freq=model_update_step_freq), i) for i in range(n_envs)])
+        
+    model = PPO("MlpPolicy", env, **model_params)
 
     #############
     #  callback
@@ -166,16 +178,21 @@ def main():
     env_eval = None
     if n_envs > 1:
         # An evaluation environment is needed to measure multi-env setups. Use a fixed 4 envs.
+        opponents = {"imitation": ImitationAgent()}
         env_eval = SubprocVecEnv([make_env(LuxEnvironment(configs=configs,
                                                      learning_agent=AgentPolicy(mode="train"),
-                                                     opponent_agent=opponent), i) for i in range(4)])
+                                                     opponent_agents=opponents), i) for i in range(1)])
         callbacks.append(EvalCallback(env_eval, **eval_params))
 
     ###########
     # train
     ###########
     logger.info("Training model...")
-    model.learn(total_timesteps=step_count, callback=callbacks)
+    if is_resume:
+        model.num_timesteps = resume_num_timesteps
+        model.learn(total_timesteps=step_count, callback=callbacks, reset_num_timesteps=False)
+    else:
+        model.learn(total_timesteps=step_count, callback=callbacks)
         
     if not os.path.exists(f'models/rl_model_{step_count}_steps.zip'):
         model.save(path=f'models/rl_model_{step_count}_steps.zip')
